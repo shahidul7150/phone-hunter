446003d6f80ef144ab8266883078c78e0bc0e8ac
Shahidul Islam <shahidulislam7150@gmail.com>
2022-03-01 20:00:08 +0600

    searching not found fixed

--- js/app.js
+++ js/app.js
@@ -14,27 +14,35 @@ const loadMobileData = () => {
 const displayMobile = (mobiles) => {
     
     console.log(mobiles);
-    const parent = document.getElementById('parent')
-    mobiles.forEach((mobile) => {
-        // console.log(mobile.slug);
-            const div=document.createElement('div')
-            div.innerHTML = `
-            <div class="col">
-             <div class="card h-100">
-                 <img src="${mobile.image}" class="card-img-top width-img mx-auto" alt="...">
-                <div class="card-body">
-                 <h6 class="card-title fs-6">${mobile.phone_name}</h6>
-                <p class="card-text">Brand: ${mobile.brand}</p>
-            </div>
-            <div class="card-footer bg-white border-0">
-        <button onclick="loadeDetailData('${mobile.slug}')" class="btn btnColor font-size">Details</button>
+    if (mobiles.length == 0) {
+        console.log('Error');
+        document.getElementById('search-err').style.display="block"
+    } else {
+        document.getElementById('search-err').text=''
+        document.getElementById('search-err').style.display="none"
+        const parent = document.getElementById('parent')
+        mobiles.forEach((mobile) => {
+            // console.log(mobile.slug);
+                const div=document.createElement('div')
+                div.innerHTML = `
+                <div class="col">
+                 <div class="card h-100">
+                     <img src="${mobile.image}" class="card-img-top width-img mx-auto" alt="...">
+                    <div class="card-body">
+                     <h6 class="card-title fs-6">${mobile.phone_name}</h6>
+                    <p class="card-text">Brand: ${mobile.brand}</p>
+                </div>
+                <div class="card-footer bg-white border-0">
+            <button onclick="loadeDetailData('${mobile.slug}')" class="btn btnColor font-size">Details</button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-         `
-        parent.appendChild(div)
-        // console.log(mobile);
-    })
+             `
+            parent.appendChild(div)
+            // console.log(mobile);
+        })
+    }
+ 
 }
 
 const loadeDetailData = (detailId) => {
